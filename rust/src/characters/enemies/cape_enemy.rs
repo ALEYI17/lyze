f01a86966cd0c6058e6f65f66cf2a9ccbc65ce40
ALEYI <aleelaleyi@gmail.com>
2026-09-06 19:54:25 -0500

    add basic hitbox and hurtbox

--- rust/src/characters/enemies/cape_enemy.rs
+++ rust/src/characters/enemies/cape_enemy.rs
@@ -1,14 +1,14 @@
 use std::sync::Mutex;
 
 use bevy::prelude::*;
-use godot::classes::{Area2D, Sprite2D, Timer};
+use godot::classes::{Area2D, CharacterBody2D, Timer};
 use godot::prelude::*;
 use godot_bevy::prelude::*;
 
 use crate::state::GameState;
 
 #[derive(Component, GodotNode, Default)]
-#[godot_node(base(Area2D), class_name(CapeEnemy))]
+#[godot_node(base(CharacterBody2D), class_name(CapeEnemy))]
 pub struct CapeEnemyNode {}
 
 const SPEED: f32 = 150.0;
@@ -24,7 +24,10 @@ struct CapeEnemyState {
 struct ChangeDirectionRequested;
 
 #[derive(Event, Debug, Clone)]
-struct EnteredBody;
+struct EnteredBody{
+    node: GodotNodeHandle,
+    entity: Option<Entity>,
+}
 
 fn connect_timeout_signal(
     signal_direction: GodotSignals<ChangeDirectionRequested>,
@@ -34,7 +37,8 @@ fn connect_timeout_signal(
     mut state: ResMut<CapeEnemyState>,
 ) {
     for handle in &mut query {
-        let Some(body) = godot.try_get::<Area2D>(*handle) else {
+        let Some(body) = godot.try_get::<CharacterBody2D>(*handle) else {
+            godot_print!("Dont get body enemy");
             return;
         };
 
@@ -48,11 +52,23 @@ fn connect_timeout_signal(
             return;
         };
 
+        let Some(hurtbox_node) = body.get_node_or_null("hitbox") else {
+            godot_print!("dont get hitbox");
+            return;
+        };
+
+        let Ok(hurtbox) = hurtbox_node.try_cast::<Area2D>() else {
+            godot_print!("cant cast hitbox");
+            return;
+        };
+
         signal_enter.connect(
-            body.into(),
-            Area2DSignals::BODY_ENTERED,
+            hurtbox.into(),
+            Area2DSignals::AREA_ENTERED,
             None,
-            |_args, _node_handle, _ent| Some(EnteredBody),
+            |_args, node_handle, ent| {
+                Some(EnteredBody { entity: ent, node: node_handle })
+            },
         );
 
         signal_direction.connect(
@@ -72,30 +88,21 @@ fn on_timeout(
     mut godot: GodotAccess,
 ) {
     for handle in &mut query {
-        let Some(body) = godot.try_get::<Area2D>(*handle) else {
+        let Some(mut body) = godot.try_get::<CharacterBody2D>(*handle) else {
             return;
         };
 
-        let Some(sprite_node) = body.get_node_or_null("Sprite2D") else {
-            godot_print!("Dont get sprite");
-            return;
-        };
 
-        let Ok(mut sprite) = sprite_node.try_cast::<Sprite2D>() else {
-            godot_print!("Cant cast sprite");
-            return;
-        };
-
-        let flip = sprite.is_flipped_h();
-        sprite.set_flip_h(!flip);
+        let scale = body.get_scale();
+        body.set_scale(Vector2::new(-scale.x, scale.y));
     }
 
     let mut direction = DIRECTION.lock().unwrap();
     *direction = *direction * -1.0;
 }
 
-fn on_enter_body(_tigger: On<EnteredBody>) {
-    godot_print!("Enter body kill player");
+fn on_enter_body(tigger: On<EnteredBody>) {
+    godot_print!("enter body kill player: {:?}, {:?}", tigger.entity, tigger.node);
 }
 
 fn is_not_initialized(state: Res<CapeEnemyState>) -> bool {
@@ -105,19 +112,25 @@ fn is_not_initialized(state: Res<CapeEnemyState>) -> bool {
 fn find_cape_enemy(
     mut query: Query<&GodotNodeHandle, With<CapeEnemyNode>>,
     mut godot: GodotAccess,
-    time: Res<Time>,
 ) {
     for handle in &mut query {
-        let Some(mut body) = godot.try_get::<Area2D>(*handle) else {
+        let Some(mut body) = godot.try_get::<CharacterBody2D>(*handle) else {
+            godot_print!("dont find enemy");
             return;
         };
 
-        let mut pos = body.get_position();
+        let mut dir = 0.0;
+
+        let mut pos = body.get_velocity();
 
         let direction = DIRECTION.lock().unwrap();
-        pos.x += *direction * SPEED * time.delta_secs();
 
-        body.set_position(pos);
+        dir += *direction;
+        pos.x = dir * SPEED;
+
+        body.set_velocity(pos);
+
+        body.move_and_slide();
     }
 }
 
